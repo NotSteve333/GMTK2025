43e4fb677a488acde27474ab7972fd6aabe234f5
NotSteve333 <alexbro0106@gmail.com>
2025-07-31 11:39:33 -0500

    loop detection

--- loop/loop_manager.gd
+++ loop/loop_manager.gd
@@ -8,22 +8,24 @@ var num_nodes = 0
 var max_nodes = 500
 
 func _process(delta):
-	var head_pos = $LoopHead.global_position
-	if trail_points.is_empty() or head_pos.distance_to(trail_points[-1]) > 20.0:
+	var head_pos = $LoopHead.position
+	
+	if (trail_interior.get_point_count() < 1) or head_pos.distance_to(trail_interior.get_point_position(min(num_nodes, max_nodes)-1)) > 20.0:
 		num_nodes += 1
-		trail_points.append(head_pos)
+		trail_interior.add_point(head_pos)
+		trail_border.add_point(head_pos)
 		var new_node = node_scenes.instantiate()
 		new_node.nontrivial_intersect.connect(recieve_intersect)
 		new_node.node_id = num_nodes
 		new_node.global_position = head_pos
 		$LoopNodes.add_child(new_node)
-		if trail_points.size() > max_nodes:
-			trail_points.remove_at(0)
-
-	trail_interior.points = trail_points
-	trail_border.points = trail_points
+		if trail_interior.get_point_count() > max_nodes:
+			trail_interior.remove_point(0)
+			trail_border.remove_point(0)
 	
 func recieve_intersect(first_id: int, second_id: int) -> void:
-	pass
+	$LoopClose/CollisionPolygon2D.polygon = trail_interior.points.slice(min(first_id, second_id), max(first_id, second_id))
 	
-	
+func _on_loop_close_body_entered(body: Node2D) -> void:
+	if body is Creature:
+		print("caught creature")
