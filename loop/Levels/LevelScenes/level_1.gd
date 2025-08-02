43756f4cc93dd1321253f875ac344adc2b4ff61d
NotSteve333 <alexbro0106@gmail.com>
2025-08-01 22:55:46 -0500

    trees :/

--- loop/Levels/LevelScenes/level_1.gd
+++ loop/Levels/LevelScenes/level_1.gd
@@ -2,14 +2,11 @@ extends Level
 
 @onready var rng = RandomNumberGenerator.new()
 
-@export var wind = Vector2.ZERO
-@export var max_wind: float = 10.0
-var dist_range = 4.0
+@export var wind = Vector2.UP
+@export var time_scale: float = 0
 
-func _process(_delta: float) -> void:
-	wind += Vector2(rng.randfn(0.0, dist_range), rng.randfn(0.0, dist_range))
-	print(wind)
-	if abs(wind.x) > max_wind:
-		wind.x = rng.randfn(0.0, dist_range)
-	if abs(wind.y) > max_wind:
-		wind.y = rng.randfn(0.0, dist_range)
+var angle_range = .1
+
+func _process(delta: float) -> void:
+	wind = wind.rotated(rng.randfn(0.0, angle_range))
+	time_scale += delta
