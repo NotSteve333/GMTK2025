45b20efb55a3500ad22148fbc3a09808246dfc62
NotSteve333 <alexbro0106@gmail.com>
2025-08-02 03:10:54 -0500

    explode

--- loop/Loop/loop_manager.gd
+++ loop/Loop/loop_manager.gd
@@ -8,11 +8,10 @@ extends Node2D
 @export var enabled = false
 @export var max_pull_speed = 500.0
 
-var trail_points: PackedVector2Array = []
 var num_nodes = 0
-var nodes = []
+var is_snapped = false
+var snap_index: int
 var active_inter: IntersectData
-var max_nodes = 500
 var delete_threshold = 15.0
 
 class IntersectData:
@@ -33,13 +32,22 @@ func _process(_delta):
 	if enabled:
 		var head_pos = $LoopHead.position
 		
-		if !head.is_stopped and ((trail_interior.get_point_count() < 1) or head_pos.distance_to(trail_interior.get_point_position(min(num_nodes, max_nodes)-1)) > 7.0):
+		if !head.is_stopped and ((trail_interior.get_point_count() < 1) or head_pos.distance_to(trail_interior.get_point_position(num_nodes - 1)) > 7.0):
 			add_loop_points(head_pos)
 		
 func _physics_process(delta: float) -> void:
 	if enabled:
-		if active_inter:
+		if is_snapped:
+			push_fuck(delta)
+		elif active_inter:
 			pull_in(active_inter, delta)
+
+func push_fuck(delta: float) -> void:
+	for j in range(num_nodes):
+		var diff = trail_border.get_point_position(j) - active_inter.center
+		var diff_m = diff.length()
+		trail_border.set_point_position(j, trail_border.get_point_position(j).move_toward(trail_border.get_point_position(j) + diff, delta * min(diff_m, max_pull_speed)))
+		trail_interior.set_point_position(j, trail_border.get_point_position(j))
 
 func recieve_intersect(first_id: int, second_id: int, center: Vector2) -> void:
 	if enabled and (active_inter == null or active_inter.finished == true):
@@ -92,4 +100,7 @@ func clear_loop() -> void:
 
 func _on_loop_close_body_entered(body: Node2D) -> void:
 	if body is Creature:
-		body.caught()
+		body.caught(active_inter.center)
+	if body is Obstacle:
+		is_snapped = true
+		active_inter.center = body.position
